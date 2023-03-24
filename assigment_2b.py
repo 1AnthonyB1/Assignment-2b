7e6de6c5383b33068b63bb85edda0de745779080
1AnthonyB1 <124379293+1AnthonyB1@users.noreply.github.com>
2023-03-24 14:06:38 +0000

    Assigment b2

---
+++ assigment_2b.py
@@ -0,0 +1,12 @@
+import math
+pi = math.pi
+d = input()
+
+r = (float(d)/2)
+print("This is the Radius:",float(r))
+
+rsquared = float(r**2)
+print("This is the Area:",pi*rsquared)
+
+twopi = (2*pi)
+print("This is the circumference:",twopi*r)
